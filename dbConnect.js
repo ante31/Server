fcc02c5a20b362a3919b2e01ad0d60311211b79c
ante31 <92745972+ante31@users.noreply.github.com>
2025-09-10 01:51:09 +0200

    Update dbConnect.js

--- dbConnect.js
+++ dbConnect.js
@@ -4,18 +4,7 @@ require('dotenv').config();
 
 const isTest = true;
 
-const firebaseConfig = isTest
-  ? {
-      apiKey: process.env.TEST_FIREBASE_API_KEY,
-      authDomain: process.env.TEST_FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.TEST_FIREBASE_DATABASE_URL,
-      projectId: process.env.TEST_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.TEST_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.TEST_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.TEST_FIREBASE_APP_ID,
-      measurementId: process.env.TEST_FIREBASE_MEASUREMENT_ID,
-    }
-  : {
+const firebaseConfig = {
       apiKey: process.env.FIREBASE_API_KEY,
       authDomain: process.env.FIREBASE_AUTH_DOMAIN,
       databaseURL: process.env.FIREBASE_DATABASE_URL,
